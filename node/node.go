b1d5e41051382505c783e5e85dbcbb557d7cf35b
Denis Tatarkin <denistatarkin11@gmail.com>
2021-09-18 22:00:50 +0300

    defer fix

--- node/node.go
+++ node/node.go
@@ -8,31 +8,47 @@ import (
 )
 
 type Proposer struct {
-	Id string
-	Address string
+	Id       string
+	Address  string
 	IsLeader bool
 }
 
-type Acceptor struct{
-	Id string
+type Acceptor struct {
+	Id      string
 	Address string
 }
 
 func ParseConfig(filename string) ([]Proposer, []Acceptor) {
-	var f,err = os.Open(filename); if err!=nil{fmt.Println(err.Error()); return nil,nil}; if f == nil{fmt.Println("No config file"); return nil,nil}
-	defer f.Close()
+	var f, err = os.Open(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, nil
+	}
+	if f == nil {
+		fmt.Println("No config file")
+		return nil, nil
+	}
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
 	var scanner = bufio.NewScanner(f)
-	var proposers[] Proposer
-	var acceptors[] Acceptor
+	var proposers []Proposer
+	var acceptors []Acceptor
 
-	for scanner.Scan(){
+	for scanner.Scan() {
 		tokens := strings.Split(scanner.Text(), " ")
 		switch tokens[2] {
-			case "proposer" : proposers = append(proposers,
-				Proposer{Id: tokens[0], Address: tokens[1], IsLeader : false})
-			case "acceptor" : acceptors = append(acceptors,
+		case "proposer":
+			proposers = append(proposers,
+				Proposer{Id: tokens[0], Address: tokens[1], IsLeader: false})
+		case "acceptor":
+			acceptors = append(acceptors,
 				Acceptor{Id: tokens[0], Address: tokens[1]})
-			default: panic("Incorrect role")
+		default:
+			panic("Incorrect role")
 		}
 	}
 
