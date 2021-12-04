f4abc04a07ace4972ade9eed3926f08e24b79abe
Denis Tatarkin <denistatarkin11@gmail.com>
2021-12-04 22:33:44 +0300

    client installing implementation

---
+++ client/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "paxos_go/pb"
+)
+
+const address = "localhost:"
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Invalid args number\ncmd port_number")
+		return
+	}
+
+	var _, err = strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal("Invalid args\nPort should be a number")
+		return
+	}
+
+	conn, err := grpc.Dial(address+os.Args[1], grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("gRPC connection creating error\n%v", err.Error())
+		return
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("gRPC connection closing error\n%v", err.Error())
+		}
+	}()
+
+	var client = pb.NewProposalExchangeClient(conn)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	//todo: console manage passing
+	log.Printf("Client estableshed successfully on the port %s", os.Args[1])
+	client.SendProposal(ctx, &pb.Proposal{
+		Key:   "",
+		Value: "",
+	})
+	//todo: console manage passing
+}
