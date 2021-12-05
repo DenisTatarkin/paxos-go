6d2fff3bde6b9bbd0bb68a7f6fe375bb224d7cac
Denis Tatarkin <denistatarkin11@gmail.com>
2021-12-05 17:12:10 +0300

    feat: CLI app supporting is implemented.

---
+++ client_app/cli/cli.go
@@ -0,0 +1,30 @@
+package cli
+
+import (
+	"context"
+	"github.com/cristalhq/acmd"
+	client "paxos_go/client"
+)
+
+type CLIapp struct {
+	cmds   []acmd.Command
+	Runner *acmd.Runner
+}
+
+func (app *CLIapp) Start() {
+	var cmds = []acmd.Command{
+		{
+			Name:        "propose",
+			Description: "Prepare and send Proposal to proposer",
+			Do:          proposeHandler,
+		},
+	}
+	app.Runner = acmd.RunnerOf(cmds, acmd.Config{
+		AppName: "paxos-cli",
+	})
+}
+
+func proposeHandler(_ context.Context, args []string) error {
+	var err = client.Execute(args[0], args[1], args[2:])
+	return err
+}
