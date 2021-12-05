c6a181382e63930bace9114059a2d9f4beab2b04
Denis Tatarkin <denistatarkin11@gmail.com>
2021-12-05 22:23:24 +0300

    proposer_service init

---
+++ proposer/service/proposer_service.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"paxos_go/pb"
+	"strconv"
+)
+
+type server struct {
+	pb.ProposalExchangeServer
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("incorrect number of arguments\ncmd port")
+	}
+
+	var _, err = strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("incorrect port: %v", err)
+	}
+
+	listener, err := net.Listen("tcp", os.Args[1])
+	if err != nil {
+		log.Fatalf("Failed to listen port %s: %v", os.Args[1], err)
+	}
+	proposalExchServer := grpc.NewServer()
+	pb.RegisterProposalExchangeServer(proposalExchServer, &server{})
+
+	log.Printf("Starting gRPC listener on port %s", os.Args[1])
+	if err := proposalExchServer.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
